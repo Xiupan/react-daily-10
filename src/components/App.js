4308200122398be30d16e0b8e82413011503900d
Alan Hong <xiupan@gmail.com>
2017-10-03 14:19:27 -0500

    Displaying all books. Still need to add click function.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../styles/App.css';
 
+import BookList from '../containers/BookList';
+import BookDetail from '../containers/BookDetail';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App
-      </div>
-    );
-  }
+    render() {
+        return (
+          <div className="container">
+            <div className="row">
+              <h1>Books N' Roses: Appetite for Instruction</h1>
+              <BookList/>
+              <BookDetail/>
+            </div>
+          </div>
+        );
+    }
 }
 
 export default App;
